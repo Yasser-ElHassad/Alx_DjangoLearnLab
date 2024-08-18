36195286d03391024dfbbb9868134634a4b2beb1
Yasser El Hassad <maroc2103maroc@gmail.com>
2024-08-18 17:48:34 +0100

    django

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -15,3 +15,11 @@ urlpatterns = [
     path('register/', views.register, name='register'),
 ]
 
+from .views import admin_view, librarian_view, member_view
+
+urlpatterns = [
+    path('admin/', admin_view, name='admin_view'),
+    path('librarian/', librarian_view, name='librarian_view'),
+    path('member/', member_view, name='member_view'),
+]
+
