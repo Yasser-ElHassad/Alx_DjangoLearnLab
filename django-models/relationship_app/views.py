594de0f0e9d456d237b01dd679ccc62cfc3d2beb
Yasser El Hassad <maroc2103maroc@gmail.com>
2024-08-18 20:20:46 +0100

    django

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -79,3 +79,26 @@ def delete_book(request, pk):
     return render(request, 'relationship_app/book_confirm_delete.html', {'book': book})
 
 
+from django.contrib.auth.decorators import user_passes_test
+
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'admin_view.html')
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'librarian_view.html')
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'member_view.html')
+
