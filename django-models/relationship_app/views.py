d948ea4a991372236ca3fc8e5f09492a63bd0bc4
Yasser El Hassad <maroc2103maroc@gmail.com>
2024-08-18 18:03:37 +0100

    django

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -40,25 +40,34 @@ def register(request):
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
 
+# admin_view.py
 from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render
 
 def is_admin(user):
-    return user.userprofile.role == 'Admin'
+    return user.is_authenticated and user.userprofile.role == 'Admin'
 
 @user_passes_test(is_admin)
 def admin_view(request):
     return render(request, 'admin_view.html')
 
+# librarian_view.py
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render
+
 def is_librarian(user):
-    return user.userprofile.role == 'Librarian'
+    return user.is_authenticated and user.userprofile.role == 'Librarian'
 
 @user_passes_test(is_librarian)
 def librarian_view(request):
     return render(request, 'librarian_view.html')
 
+# member_view.py
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render
 
 def is_member(user):
-    return user.userprofile.role == 'Member'
+    return user.is_authenticated and user.userprofile.role == 'Member'
 
 @user_passes_test(is_member)
 def member_view(request):
