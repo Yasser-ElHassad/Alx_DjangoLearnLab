3e492eebcff547e83a093c193fc71a1e015d0b2a
Yasser El Hassad <maroc2103maroc@gmail.com>
2024-08-18 18:11:27 +0100

    django

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -24,3 +24,11 @@ urlpatterns = [
     path('member-view/', member_view.member_view, name='member_view'),
 ]
 
+from .views import add_book, edit_book, delete_book
+
+urlpatterns = [
+    path('book/add/', add_book, name='add_book'),
+    path('book/edit/<int:pk>/', edit_book, name='edit_book'),
+    path('book/delete/<int:pk>/', delete_book, name='delete_book'),
+]
+
