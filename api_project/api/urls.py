ffd815c1df26f77852c744112534a33bf49b2a3b
Yasser El Hassad <maroc2103maroc@gmail.com>
2024-08-30 14:29:32 +0100

    api

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,5 +1,5 @@
 # api/urls.py
-from django.urls import path
+from django.urls import path, include
 from .views import BookList
 from rest_framework.routers import DefaultRouter
 from .views import BookViewSet
@@ -12,5 +12,5 @@ router.register(r'books', BookViewSet)
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
-    router.urls,
+    path('', include(router.urls)),
 ]
