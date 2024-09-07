02105af6e5792d02cd042f9a4abdfcebc54012f8
Yasser El Hassad <maroc2103maroc@gmail.com>
2024-09-07 18:32:52 +0100

    django

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
-
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
