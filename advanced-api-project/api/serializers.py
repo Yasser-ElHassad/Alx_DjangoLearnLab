f58dd1f8436bbf19b7266c039571645f71b52f80
Yasser El Hassad <maroc2103maroc@gmail.com>
2024-09-07 16:58:14 +0100

    django

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -1,3 +1,4 @@
+import datetime
 from rest_framework import serializers
 from .models import Author, Book
 
@@ -5,6 +6,11 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = '__all__'
+
+    def validate_publication_year(self, value):
+        if value > datetime.date.today().year:
+            raise serializers.ValidationError("Publication year cannot be in the future.")
+        return value
 
 class AuthorSerializer(serializers.ModelSerializer):
     books = BookSerializer(many=True, read_only=True)
