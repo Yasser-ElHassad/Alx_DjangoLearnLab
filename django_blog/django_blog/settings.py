38fa1dc329ce06dc9f656c2e7bbe8259991c670d
Yasser El Hassad <maroc2103maroc@gmail.com>
2024-09-15 14:17:06 +0100

    django

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'your_database_name',
+        'USER': 'your_database_user',
+        'PASSWORD': 'your_database_password',
+        'HOST': 'localhost',  # Or the IP address of your database server
+        'PORT': '5432',  # Default PostgreSQL port
     }
 }
 
