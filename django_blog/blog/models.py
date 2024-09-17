dd833ad54eeeee31f1b7b02d59c287b3e6bd6bcb
Yasser El Hassad <maroc2103maroc@gmail.com>
2024-09-17 22:59:50 +0100

    django

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from taggit.managers import TaggableManager
+
 class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
 
@@ -11,7 +12,7 @@ class Tag(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     tags = TaggableManager()
