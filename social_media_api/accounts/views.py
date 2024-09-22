85bc77333c3b81ca37e405841d06d42003770323
Yasser El Hassad <maroc2103maroc@gmail.com>
2024-09-22 15:55:43 +0100

    django

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -3,10 +3,13 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from .serializers import UserSerializer
-from rest_framework import status
-from django.shortcuts import get_object_or_404
 from .models import CustomUser
 from rest_framework import generics
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from .models import Post
+from .serializers import PostSerializer
+from .permissions import IsAuthorOrReadOnly
 
 class RegisterView(APIView):
     def post(self, request):
@@ -17,17 +20,12 @@ class RegisterView(APIView):
             return Response({'token': token.key}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-from rest_framework import generics
-from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
-from .models import Post
-from .serializers import PostSerializer
-from .permissions import IsAuthorOrReadOnly
 
-class FollowUserView(generics.ListCreateAPIView):
+
+class FollowUserView(generics.CreateAPIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
-    queryset = Post.objects.all()
+    queryset = CustomUser.objects.all()
     serializer_class = PostSerializer
 
     def perform_create(self, serializer):
@@ -36,7 +34,7 @@ class FollowUserView(generics.ListCreateAPIView):
 class UnfollowUserView(generics.RetrieveUpdateDestroyAPIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
-    queryset = Post.objects.all()
+    queryset = CustomUser.objects.all()
     serializer_class = PostSerializer
 
 
