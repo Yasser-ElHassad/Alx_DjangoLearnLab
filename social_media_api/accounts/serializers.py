699b93eea580954fabd064bb8e9330ea8fe15cd7
Yasser El Hassad <maroc2103maroc@gmail.com>
2024-09-22 13:07:56 +0100

    django

---
+++ social_media_api/accounts/serializers.py
@@ -0,0 +1,26 @@
+# accounts/serializers.py
+
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password', 'bio')
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            bio=validated_data.get('bio', '')
+        )
+        return user
+
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
