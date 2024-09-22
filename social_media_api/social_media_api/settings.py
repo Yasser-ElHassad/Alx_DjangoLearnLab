79c5c9c732355e33f0ea129cbcf2740cfa55d2da
Yasser El Hassad <maroc2103maroc@gmail.com>
2024-09-22 18:42:25 +0100

    django

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -78,10 +78,15 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',  # or 'django.db.backends.mysql'
+        'NAME': 'your_database_name',
+        'USER': 'your_database_user',
+        'PASSWORD': 'your_database_password',
+        'HOST': 'localhost',  # e.g., 'localhost' or an IP address
+        'PORT': '5432',  # e.g., '5432' for PostgreSQL or '3306' for MySQL
     }
 }
+
 
 
 # Password validation
